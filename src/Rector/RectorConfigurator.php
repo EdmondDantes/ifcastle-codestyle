1ac1de1771a03c6cd7863aa7d17941c7dd22bb38
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-10-26 12:22:11 +0300

    % refactoring Rector rules to SKIP method

--- src/Rector/RectorConfigurator.php
+++ src/Rector/RectorConfigurator.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace IfCastle\CodeStyle\Rector;
 
+use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
+use Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector;
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
+use Rector\CodingStyle\Rector\Encapsed\WrapEncapsedVariableInCurlyBracesRector;
+use Rector\CodingStyle\Rector\String_\SymplifyQuoteEscapeRector;
 use Rector\Config\RectorConfig;
+use Rector\EarlyReturn\Rector\If_\RemoveAlwaysElseRector;
+use Rector\Php74\Rector\Property\RestoreDefaultNullToNullableTypePropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-
 
 class RectorConfigurator
 {
@@ -15,12 +21,31 @@ class RectorConfigurator
     {
         $rectorConfig->sets([
             LevelSetList::UP_TO_PHP_83,
-            __DIR__ . '/coding-style.php',
-            __DIR__ . '/code-quality.php',
-            __DIR__ . '/type-declaration.php',
-            __DIR__ . '/instanceof.php',
-            __DIR__ . '/early-return.php',
-            SetList::DEAD_CODE
+            SetList::CODING_STYLE,
+            SetList::CODE_QUALITY,
+            SetList::TYPE_DECLARATION,
+            SetList::INSTANCEOF,
+            SetList::EARLY_RETURN,
+            SetList::DEAD_CODE,
+        ]);
+        
+        $rectorConfig->skip([
+            RestoreDefaultNullToNullableTypePropertyRector::class,
+            FlipTypeControlToUseExclusiveTypeRector::class,
+            SymplifyQuoteEscapeRector::class,
+            CatchExceptionNameMatchingTypeRector::class,
+            EncapsedStringsToSprintfRector::class,
+            WrapEncapsedVariableInCurlyBracesRector::class,
+            // Note: RemoveAlwaysElseRector not understood when parameter has different interfaces
+            RemoveAlwaysElseRector::class,
+            FlipTypeControlToUseExclusiveTypeRector::class,
+        ]);
+    }
+    
+    public static function skipForTests(string $testDir, RectorConfig $rectorConfig): void
+    {
+        $rectorConfig->skip([
+        
         ]);
     }
 }
